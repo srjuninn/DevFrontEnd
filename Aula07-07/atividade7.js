3023b8e8c02525b739e03bc6567593aaf83217a6
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-07-07 15:28:28 -0300

    feat: add motivational message function with custom phrase

---
+++ Aula07-07/atividade7.js
@@ -0,0 +1,6 @@
+// Crie uma função chamada mensagemMotivacional() que mostre no console uma frase motivacional, como:
+// "Você é capaz de aprender qualquer coisa! 💪"
+function mensagemMotivacional(){
+    console.log("Você é capaz de aprender qualquer coisa..... Seu frango 🐔");
+}
+mensagemMotivacional();
