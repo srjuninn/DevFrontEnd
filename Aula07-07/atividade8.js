5e601232f2cb180bf07df626f1095fd9ae53b4ce
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-07-07 15:29:00 -0300

    feat: add function to calculate and display the double of a number

---
+++ Aula07-07/atividade8.js
@@ -0,0 +1,7 @@
+// EX04
+// Crie uma função chamada dobro(numero) que receba um número como parâmetro e exiba o dobro desse número no console.
+function dobro(numero){
+    console.log(`O dobro do número que você inseriu é: ${numero * 2}`)
+}
+//insira algum número nos parentesês pra ver o dobro dele
+dobro(10)
