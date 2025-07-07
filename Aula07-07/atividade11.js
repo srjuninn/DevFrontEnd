35e1054f18b0f7d8492ab38ebd98a42373208314
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-07-07 16:24:25 -0300

    feat: add arrow function to multiply two numbers

---
+++ Aula07-07/atividade11.js
@@ -0,0 +1,6 @@
+// EX1 - Criando uma Arrow Function para Multiplicação
+// - Crie uma arrow function chamada `multiplicar` que recebe dois números. 
+// - Ela deve retornar a multiplicação dos dois valores. 
+// - Teste chamando `multiplicar(7, 3)`.
+const multiple = (n1, n2) => console.log(`A multiplicação dos dois números são: ${n1*n2}`)
+multiple(5,5);
