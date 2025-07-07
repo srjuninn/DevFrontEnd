76b3a4b5b8abc9c51f7ba758ad1ba87d698c188d
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-07-07 15:27:45 -0300

    feat: add function to display course name in console

---
+++ Aula07-07/atividade6.js
@@ -0,0 +1,6 @@
+// Ex02
+// Crie uma função chamada mostrarCurso() que exibe no console: "Curso: Desenvolvimento Web com JavaScript"
+function mostrarCurso(){
+    console.log("Curso: Denvolvimento web com JavaScript");
+}
+mostrarCurso();
